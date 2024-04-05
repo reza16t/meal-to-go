e25638b1c3fcdbab7ab7a96117754f37f76439d3
reza swary <126505674+reza18s@users.noreply.github.com>
2024-04-05 20:24:33 +0330

    Update firebase.js

--- src/API/firebase.js
+++ src/API/firebase.js
@@ -1,15 +1,7 @@
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
-const firebaseConfig = {
-   apiKey: "AIzaSyAY3nl54NoZcpnTa1vmKh49Gf8jliRTJrc",
-   authDomain: "meals-to-go-bcfc3.firebaseapp.com",
-   projectId: "meals-to-go-bcfc3",
-   storageBucket: "meals-to-go-bcfc3.appspot.com",
-   messagingSenderId: "994676785530",
-   appId: "1:994676785530:web:829d761b87ba2db9d1d6c3",
-   measurementId: "G-CDNT048GE4",
-};
+
 
 // Initialize Firebase
 // if (!getApps.length) {
